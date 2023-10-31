4363ec476ebd48ac7a1142c3faa6bb21e1df56c4
Danishtota <149455054+Danishtota@users.noreply.github.com>
2023-10-31 09:10:01 +0500

    Login sign up has been completed

---
+++ registers.php
@@ -0,0 +1,67 @@
+
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $user_type = $_POST['user_type'];
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+   
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>register now</h3>
+
+      <input type="text" name="name" placeholder="Full Name" required class="box">
+      <input type="email" name="email" placeholder="Email" required class="box">
+      <input type="password" name="password" placeholder="Password" required class="box">
+      <input type="password" name="cpassword" placeholder="Confirm password" required class="box">
+      <select name="user_type" class="box">
+         <option value="user">user</option>
+         <option value="admin">admin</option>
+      </select>
+      
+      <input type="submit" name="submit" value="Register now" class="btn">
+      <p>Already have an account? &nbsp  <a href="login.php">login now</a></p>
+   </form>
+</div>
+
+</body>
+</html>
